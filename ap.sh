41b71c18bcec4912f75b2fd2d010e078e7c6606f
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2025-04-15 12:34:01 +0200

    m

---
+++ ap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo ip addr add 192.168.1.1/24 dev br0
+sudo ip link set dev wlan0 master br0
+sudo ip link set dev br0 up
+sudo sysctl -w net.ipv4.ip_forward=1
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sudo iptables -A FORWARD -i br0 -o eth0 -j ACCEPT
+sudo iptables -A FORWARD -i eth0 -o br0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo systemctl start hostapd
+sudo systemctl start dnsmasq
