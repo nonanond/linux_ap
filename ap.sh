e039d5ec20622da5fb8ec397e5d59c45dd3734d9
nonanond <non1337anon@outlook.com>
2025-04-22 13:59:55 +0200

    Update ap.sh

--- ap.sh
+++ ap.sh
@@ -8,3 +8,9 @@ sudo iptables -A FORWARD -i br0 -o eth0 -j ACCEPT
 sudo iptables -A FORWARD -i eth0 -o br0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 sudo systemctl start hostapd
 sudo systemctl start dnsmasq
+
+# sudo ip addr add 192.168.0.1/24 dev enp3s0
+# sudo ip link set dev enp3s0 up
+# sudo iptables -t nat -A POSTROUTING -o wlp2s0 -j MASQUERADE
+# sudo iptables -A FOWARD -i enp3s0 -o wlp2s0 -j ACCEPT
+# sudo iptables -A FORWARD -i wlp2s0 -o enp3s0 -m state --state RELATED,ESTABLISHED -j ACCEPT
